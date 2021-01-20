d047ecf804f0155de3ffe628f68bd03844fb849b
Jerome Nicco <jeromenicco@MacBook-Pro-de-Jerome.local>
2021-01-20 23:52:19 +0100

    build

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react"
+import React, { useState } from "react"
 import { Switch, Route, withRouter, useLocation } from "react-router-dom"
 import { TransitionGroup, CSSTransition } from "react-transition-group"
 
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { NavLink as Link } from "react-router-dom";
 import { menuItems } from '../App'
 import { ContextState } from "../ContextState";
